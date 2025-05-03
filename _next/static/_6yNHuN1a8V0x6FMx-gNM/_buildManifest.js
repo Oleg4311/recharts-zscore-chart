122abd4ed59a16d5148cdec49716b0b55745e77a
Oleg4311 <olegbelii4311@gmail.com>
2025-05-03 15:45:11 +0300

    Updates

---
+++ _next/static/_6yNHuN1a8V0x6FMx-gNM/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/38-0c78c8b4f986851b.js","static/chunks/pages/index-330d84357fdec4cd.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
